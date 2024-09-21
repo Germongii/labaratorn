3973dff7c1258d4825241f0f93cf64c87654e409
vilnus <90980192+Germongii@users.noreply.github.com>
2024-09-21 16:28:04 +0300

    Сделано 2 задание 1 лабараторной

--- lab1/Ex2/ex2/ConsoleApplication1/ConsoleApplication1.cpp
+++ lab1/Ex2/ex2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,9 +2,23 @@
 //
 
 #include <iostream>
-
+#include <string>
+#include <windows.h>
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleOutputCP(CP_UTF8); 
+
+    int a = 150;
+    float b = 15.933;
+    unsigned char c = 250;
+    std::cout << "a = " << a << " b = " << b << " c = " << static_cast<int>(c);
+    int day = 9;
+    std::string month = u8"мая";
+    int year = 2005;
+    std::cout << u8"\nМоя дата рождения: " << day << " " << month << " " << year << u8" года \n";
+    const double first = 2.3;
+    const std::string second = "WINDOWS";
+    std::cout << first << " " << second;
+    
 }
 
